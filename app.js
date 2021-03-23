0636a0e572a0a693e28eecca9a253e206fcce19f
Yohan Fleury <yfleury95@gmail.com>
2021-03-23 18:35:32 +0100

    pushing commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes/user')
+
+const path = require('path')
+
+mongoose.connect('mongodb+srv://yoyo:0JXfTr6lFMC7UI0l@cluster0.1atd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  
+{ useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json())
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes)
+
+
+
+
+module.exports = app;
